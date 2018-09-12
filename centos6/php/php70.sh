72b382a1d458bce626ac564d3c295c68f18fb15b
计岷东 <jmd025@gmail.com>
2018-09-12 21:42:11 +0800

    1.centos6/php/php70.sh

---
+++ centos6/php/php70.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+yum remove php*
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+yum -y install http://rpms.remirepo.net/enterprise/remi-release-6.rpm
+
+yum -y install yum-utils
+yum-config-manager --enable remi-php70 
+
+yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo php-fpm php-mbstring php-pecl-igbinary php-pecl-jsonc php-pecl-redis php-process php-xml php-pdo php-pecl-event
